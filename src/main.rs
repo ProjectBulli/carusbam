66c120390c633824eb8aa388066638a3b2af57c7
Bernd Dutkowski <dutkowski@synyx.de>
2019-03-10 01:33:26 +0100

    learning rust and removing non idiomatic code

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ extern crate libusb;
 use std::time::Duration;
 use libusb::{Device, DeviceHandle, Direction, Error, Recipient, request_type, RequestType};
 
-// OAP Control requests:
-const ACC_REQ_GET_PROTOCOL :u8 = 51;
-const ACC_REQ_SEND_STRING :u8 = 52;
-const ACC_REQ_START :u8 = 53;
+const GET_PROTOCOL:u8 = 51;
+const SEND_STRING:u8 = 52;
+const REQUEST_START:u8 = 53;
 
 fn main() {
     match libusb::Context::new() {
@@ -27,21 +26,9 @@ fn main() {
 fn prope_device(device: Device) {
     match device.open() {
         Ok(handle) => {
-            match device.device_descriptor() {
-                Ok(device_desc) => {
-                    println!("Bus {:03} Device {:03} ID {:04x}:{:04x}",
-                             device.bus_number(),
-                             device.address(),
-                             device_desc.vendor_id(),
-                             device_desc.product_id());
-
-                    println!("{:?}", read(handle));
-
-                }
-                Err(e) => {println!("Error {}", e)}
-            }
+            println!("{:?}", read(handle));
         }
-        Err(_e) => {}
+        Err(e) => {println!("Error {}", e)}
     }
 }
 
@@ -51,7 +38,7 @@ fn read(handle: DeviceHandle) -> Result<usize, Error> {
     let write_request = request_type(Direction::Out, RequestType::Vendor, Recipient::Device);
     let timeout = Duration::from_secs(1);
 
-    return handle.read_control(read_request, ACC_REQ_GET_PROTOCOL, 0, 0, &mut buffer, timeout)
+    return handle.read_control(read_request, GET_PROTOCOL, 0, 0, &mut buffer, timeout)
         .and_then(|_| {
             let version = buffer.as_version();
             println!("Version '{}'", version);
@@ -61,9 +48,9 @@ fn read(handle: DeviceHandle) -> Result<usize, Error> {
                 Result::Ok(0)
             }
         })
-        .and_then(|_| handle.write_control(write_request, ACC_REQ_SEND_STRING, 0, 0, "Android".as_ref(), timeout))
-        .and_then(|_| handle.write_control(write_request, ACC_REQ_SEND_STRING, 0, 1, "Android Auto".as_ref(), timeout))
-        .and_then(|_| handle.write_control(write_request, ACC_REQ_START, 0, 0, &mut [], timeout));
+        .and_then(|_| handle.write_control(write_request, SEND_STRING, 0, 0, "Android".as_ref(), timeout))
+        .and_then(|_| handle.write_control(write_request, SEND_STRING, 0, 1, "Android Auto".as_ref(), timeout))
+        .and_then(|_| handle.write_control(write_request, REQUEST_START, 0, 0, &mut [], timeout));
 }
 
 trait VersionPackage {
