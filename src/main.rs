e505b074f17896739d844bc8df56ae06b37ec408
nexoscp <nexoscp@users.noreply.github.com>
2019-04-18 01:32:53 +0200

    error handling, get bus number and device number from args

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 extern crate libusb;
 
 use std::time::Duration;
-use libusb::{Context, Device, Direction, Error, Recipient, request_type, RequestType};
+use libusb::{Context, Device, Direction, Recipient, request_type, RequestType};
+use std::env;
+use std::num::ParseIntError;
 
 const GET_PROTOCOL: u8 = 51;
 const SEND_STRING: u8 = 52;
 const REQUEST_START: u8 = 53;
 
+#[derive(Debug)]
+enum Error {
+    USB(libusb::Error),
+    Parse(ParseIntError),
+    Args(Vec<String>),
+    UnsupportedVersion(u16)
+}
+
 fn main() -> Result<(), Error> {
-    let context = Context::new()?;
-    for device in context.devices()?.iter() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 2 {
+        return Err(Error::Args(args))
+    }
+    let bus_number = &args[1].parse::<u8>().map_err(Error::Parse)?;
+    let device_number = &args[2].parse::<u8>().map_err( Error::Parse)?;
+    let context = Context::new().map_err( Error::USB)?;
+    for device in context.devices().map_err(Error::USB)?.iter() {
         let bus = device.bus_number();
         let address = device.address();
         match probe_device(device) {
@@ -17,7 +33,7 @@ fn main() -> Result<(), Error> {
                 println!("   ok {}:{} version {}", bus, address, version)
             }
             Err(e) => {
-                println!("error {}:{} {}", bus, address, e)
+                println!("error {}:{} {:?}", bus, address, e)
             }
         }
     }
@@ -25,20 +41,20 @@ fn main() -> Result<(), Error> {
 }
 
 fn probe_device(device: Device) -> Result<u16, Error> {
-    let handle = device.open()?;
+    let handle = device.open().map_err( Error::USB)?;
     let mut buffer: [u8; 2] = [0, 2];
     let read_request = request_type(Direction::In, RequestType::Vendor, Recipient::Device);
     let write_request = request_type(Direction::Out, RequestType::Vendor, Recipient::Device);
     let timeout = Duration::from_secs(1);
 
-    handle.read_control(read_request, GET_PROTOCOL, 0, 0, &mut buffer, timeout)?;
+    handle.read_control(read_request, GET_PROTOCOL, 0, 0, &mut buffer, timeout).map_err(Error::USB)?;
     let version = buffer.as_version();
     if version < 1 {
-        return Result::Err(Error::NotSupported);
+        return Err(Error::UnsupportedVersion(version));
     }
-    handle.write_control(write_request, SEND_STRING, 0, 0, "Android".as_ref(), timeout)?;
-    handle.write_control(write_request, SEND_STRING, 0, 1, "Android Auto".as_ref(), timeout)?;
-    handle.write_control(write_request, REQUEST_START, 0, 0, &mut [], timeout)?;
+    handle.write_control(write_request, SEND_STRING, 0, 0, "Android".as_ref(), timeout).map_err(Error::USB)?;
+    handle.write_control(write_request, SEND_STRING, 0, 1, "Android Auto".as_ref(), timeout).map_err(Error::USB)?;
+    handle.write_control(write_request, REQUEST_START, 0, 0, &mut [], timeout).map_err(Error::USB)?;
     Ok(version)
 }
 
@@ -48,6 +64,6 @@ trait VersionPackage {
 
 impl VersionPackage for [u8; 2] {
     fn as_version(&self) -> u16 {
-        (self[1] as u16) << 8 | (self[0] as u16)
+        u16::from(self[1]) << 8 | u16::from(self[0])
     }
 }
