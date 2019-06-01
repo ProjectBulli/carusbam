7bca9096369a5159adffc7213f41d8ca27c2f59a
nexoscp <nexoscp@users.noreply.github.com>
2019-06-01 19:41:39 +0200

    cleanup build

--- build.install.sh
+++ build.install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 ./build.release.sh
 sudo dpkg --remove carusbam
-#sudo dpkg -i target/x86_64-unknown-linux-musl/debian/carusbam_0.1.0_amd64.deb
-sudo dpkg -i target/x86_64-unknown-linux-gnu/debian/carusbam_0.1.0_amd64.deb
+sudo dpkg -i target/debian/carusbam_0.1.0_amd64.deb
 sudo udevadm control --reload-rules
-# sudo udevadm trigger
--- build.release.sh
+++ build.release.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 export PKG_CONFIG_ALLOW_CROSS=1
-rustup target add x86_64-unknown-linux-musl
-#rustup target add armv7-unknown-linux-musleabihf # rPi 32 bit
-#rustup target add armv7-unknown-linux-gnueabihf # rPi 32 bit
-#export RUSTFLAGS='-C prefer-dynamic'
 export RUSTFLAGS='-C link-arg=-s' #strip debug information
-#cargo deb # --target=x86_64-unknown-linux-musl
-cargo deb --target=x86_64-unknown-linux-gnu
-#cargo deb --target=armv7-unknown-linux-gnueabihf
+cargo deb
 ls -Ss1pq --block-size=1024 target/*/release/carusbam
-ls -Ss1pq --block-size=1024 target/*/debian/*.deb
-dpkg-deb -c target/*/debian/*.deb
+ls -Ss1pq --block-size=1024 target/debian/*.deb
+dpkg-deb -c target/debian/*.deb
